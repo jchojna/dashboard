9d96d52ad9907a96350725e05d501664a959a725
jchojna <jakub.chojna@gmail.com>
2020-04-21 20:20:38 +0200

    feat(TextPanel): add TextPanel component and set its basic structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
+import TextPanel from './TextPanel';
 import '../scss/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App__header">
-
-      </header>
+        <h1 className="App__heading">Enterprise Shiny Dashboards</h1>
+        <TextPanel />
     </div>
   );
 }
---
+++ src/components/TextPanel.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../scss/TextPanel.scss";
+
+const TextPanel = (props) => {
+  const {heading, value, percentage} = props;
+  return (
+    <section className="TextPanel">
+      <h2 className="TextPanel__heading">{heading}</h2>
+      <p className="TextPanel__value">`$ ${value}`</p>
+      <p className="TextPanel__percentage">`${percentage}%`</p>
+      <nav className="TextPanel__footer">
+        <select name="stats" className="TextPanel__dropdown">
+          <option value="weekly">Weekly Stats</option>
+          <option value="monthly">Monthly Stats</option>
+          <option value="yearly">Yearly Stats</option>
+        </select>
+        {/* <svg className="TextPanel__icon" viewBox="0 0 100 100">
+          <use href=""></use>
+        </svg> */}
+      </nav>
+    </section>
+  );
+};
+export default TextPanel;
