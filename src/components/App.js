17d9839fedcf4506aa7bbb2931d22da49b7654fc
jchojna <jakub.chojna@gmail.com>
2020-04-21 18:46:53 +0200

    chore: set initial project's structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../scss/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
