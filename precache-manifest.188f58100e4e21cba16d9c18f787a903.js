8a8e12b1dfb37c4990f3f6459bbd50d88b5d2ef1
jchojna <jakub.chojna@gmail.com>
2020-04-26 16:43:53 +0200

    Updates

---
+++ precache-manifest.188f58100e4e21cba16d9c18f787a903.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34010b27656e0387bdec17a4ce245489",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "391ce6f93943fe083741",
+    "url": "/dashboard/static/css/main.c314fc06.chunk.css"
+  },
+  {
+    "revision": "958803dfa4cab9bc6181",
+    "url": "/dashboard/static/js/2.6d7daf08.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dashboard/static/js/2.6d7daf08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "391ce6f93943fe083741",
+    "url": "/dashboard/static/js/main.f62b3f81.chunk.js"
+  },
+  {
+    "revision": "1e4676832634949bc484",
+    "url": "/dashboard/static/js/runtime-main.2ad4d2a9.js"
+  },
+  {
+    "revision": "d78c54d1f8427606a48130c4bd53c8c1",
+    "url": "/dashboard/static/media/icons.d78c54d1.svg"
+  }
+]);
